2dd7d416435cba4147da09a5e9f1c145e36c5cd4
hanqf <hanqf2008@163.com>
2016-12-01 18:46:34 +0800

    修改应用名称，新增数据脚本文件

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `tbl_demo` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SELECT * FROM test.tbl_demo;
